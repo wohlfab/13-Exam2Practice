7b2e281e28e8e267e86854ac1930c2af73334a43
wohlfab <wohlfab@rose-hulman.edu>
2019-01-07 15:18:28 -0500

    Exam 2 prep

--- src/practice_problem2.py
+++ src/practice_problem2.py
@@ -210,7 +210,7 @@ def practice_problem2b(sequence):
       :type sequence: [str]
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
